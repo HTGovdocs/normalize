f45791bd718867389677cd829eaa56e972d830f6
Josh Steverman <jstever@umich.edu>
2015-02-06 11:15:01 -0500

    expanding USGPO to United States... education office

--- lib/normalize_corporate.rb
+++ lib/normalize_corporate.rb
@@ -10,18 +10,24 @@ def normalize_corporate( corp )
   ag.gsub!(/DEPARTMENT/, 'DEPT');
   ag.gsub!(/DEPTOF/, 'DEPT OF'); # Strangely common typo(?)
 
-  ag.gsub!(/&(AMP)?/, ' AND ');
+  ag.gsub!(/&(AMP)?(#X26)?/, ' AND ');
+  
 
   ag.gsub!(/UNITED STATES( OF AMERICA)?/, 'UNITED STATES');
   ag.gsub!(/\bUS\b/, 'UNITED STATES'); #dangerous?
+  ag.gsub!(/\bUSA\b/, 'UNITED STATES'); #dangerous?
   ag.gsub!(/U\sS\s|U S$/, 'UNITED STATES ');
+  ag.gsub!(/GOV'T/, 'GOVERNMENT');
   ag.gsub!(/GOVT/, 'GOVERNMENT');
   ag.gsub!(/ SPN$/, '');
+  ag.gsub!(/HOUSE( OF)? REPRESENTATIVES/, 'HOUSE ');
 
   # US GOVT PRINT OFF, which is so common yet has so many variations.
-  ag.sub!(/(US\s?)?GOVT\s?PRINT(ING)?\s?OFF(ICE)?/, 'USGPO');
-  ag.sub!(/U\s?S\s?G\s?P\s?O/, 'USGPO');
-  ag.sub!(/^GPO$/, 'USGPO');
+  ag.sub!(/(US\s?)?GOVT\s?PRINT(ING)?\s?OFF(ICE)?/, 'UNITED STATES GOVERNMENT PRINTING OFFICE');
+  ag.sub!(/U\s?S\s?G\s?P\s?O/, 'UNITED STATES GOVERNMENT PRINTING OFFICE');
+  ag.sub!(/^GPO$/, 'UNITED STATES GOVERNMENT PRINTING OFFICE');
+  ag.sub!(/UNITED STATES GOVERNMENT PRINT OFF/, 'UNITED STATES GOVERNMENT PRINTING OFFICE');
+  
   
   #mostly congressional stuff
   ag.sub!(/(\d?)2D/, '\12ND'); 
@@ -32,6 +38,9 @@ def normalize_corporate( corp )
 
   #authorities drop the inc, so we probably should too
   ag.sub!(/\b(INC)$/, ''); 
+
+  #one off fixes
+  ag.sub!(/^UNITED STATES EDUCATION OFFICE$/, 'UNITED STATES OFFICE OF EDUCATION')
 
 
   ag.gsub!(/ +/, ' '); # whitespace
