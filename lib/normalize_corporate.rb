e78ba15d5c2980574c42e90113a04df482e3e68a
Josh Steverman <jstever@umich.edu>
2015-02-09 11:08:30 -0500

    different normalization process for fulltext 110 and subfields, standardization of DEPT/OFFICE/BUREAU

--- lib/normalize_corporate.rb
+++ lib/normalize_corporate.rb
@@ -1,5 +1,5 @@
 
-def normalize_corporate( corp )
+def normalize_corporate( corp, subfield=True )
   ag = corp.upcase; 
   ag.gsub!(/[,\.:;]|\'S?/, '');   # punctuations
   ag.gsub!(/[\(\)\{\}\[\]]/, ''); # Brackets
@@ -13,6 +13,7 @@ def normalize_corporate( corp )
   ag.gsub!(/&(AMP)?(#X26)?/, ' AND ');
   
 
+  ag.gsub!(/UNITED TATES/, 'UNITED STATES');
   ag.gsub!(/UNITED STATES( OF AMERICA)?/, 'UNITED STATES');
   ag.gsub!(/\bUS\b/, 'UNITED STATES'); #dangerous?
   ag.gsub!(/\bUSA\b/, 'UNITED STATES'); #dangerous?
@@ -42,31 +43,32 @@ def normalize_corporate( corp )
   #authorities drop the inc, so we probably should too
   ag.sub!(/\b(INC)$/, ''); 
 
-  if ag !~ /COMMITTEE/ and ag !~ /OFFICE/ and ag !~ /CAUCUS/  \
-     and ag !~ /CONFEDERATE/ and ag !~ /STUDY/ #overreach?
-    ag.sub!(/HOUSE /, 'HOUSE COMMITTEE ON ');
-    ag.sub!(/SENATE /, 'SENATE COMMITTEE ON ');
-  else
-    ag.sub!(/UNITED STATES (CONGRESS )?(HOUSE |SENATE )(.*) COMMITTEE$/, 'UNITED STATES \1\2COMMITTEE ON \3');
+  unless subfield #only try this stuff if we are working on a full 110
+    if ag !~ /COMMITTEE/ and ag !~ /OFFICE/ and ag !~ /CAUCUS/  \
+       and ag !~ /CONFEDERATE/ and ag !~ /STUDY/ #overreach?
+      ag.sub!(/HOUSE /, 'HOUSE COMMITTEE ON ');
+      ag.sub!(/SENATE /, 'SENATE COMMITTEE ON ');
+    else
+      ag.sub!(/UNITED STATES (CONGRESS )?(HOUSE |SENATE )(.*) COMMITTEE$/, 'UNITED STATES \1\2COMMITTEE ON \3');
+    end
+
+    ag.sub!(/^UNITED STATES EDUCATION OFFICE$/, 'UNITED STATES OFFICE OF EDUCATION');
+    ag.sub!(/^UNITED STATES ENGINEERS CORPS$/, 'UNITED STATES ARMY CORPS OF ENGINEERS');
+    ag.sub!(/UNITED STATES ENGINEERS CORPS ARMY$/, 'UNITED STATES ARMY CORPS OF ENGINEERS');
+    ag.sub!(/^LIBRARY OF CONGRESS$/, 'UNITED STATES LIBRARY OF CONGRESS'); #librarians think they're special
   end
 
+  if subfield #only try this stuff for the subfields
+    ag.sub!(/^(.*) (DEPT|OFFICE|BUREAU)$/, '\2 OF \1'); #those three at the start. VIAFs aren't standard on this point, but we are
+  end
+  
   #oops
   ag.sub!(/UNITED STATES UNITED STATES/, 'UNITED STATES');
 
   #one off fixes
-  ag.sub!(/^UNITED STATES EDUCATION OFFICE$/, 'UNITED STATES OFFICE OF EDUCATION');
-  ag.sub!(/^UNITED STATES ENGINEERS CORPS$/, 'UNITED STATES ARMY CORPS OF ENGINEERS');
-  ag.sub!(/UNITED STATES ENGINEERS CORPS ARMY$/, 'UNITED STATES ARMY CORPS OF ENGINEERS');
   ag.sub!(/ATOMIC ENERGY JOINT COMMITTEE/, 'JOINT COMMITTEE ON ATOMIC ENERGY');
   ag.sub!(/ECONOMIC JOINT COMMITTEE/, 'JOINT ECONOMIC COMMITTEE');
-  ag.sub!(/^UNITED STATES LIBRARY OF CONGRESS$/, 'LIBRARY OF CONGRESS'); #librarians think they're special
 
-  ag.sub!(/UNITED STATES STATE DEPT\b/, 'UNITED STATES DEPT OF STATE');
-  ag.sub!(/UNITED STATES JUSTICE DEPT\b/, 'UNITED STATES DEPT OF JUSTICE');
-  ag.sub!(/UNITED STATES INTERIOR DEPT\b/, 'UNITED STATES DEPT OF INTERIOR');
-  ag.sub!(/UNITED STATES COMMERCE DEPT\b/, 'UNITED STATES DEPT OF COMMERCE');
-  ag.sub!(/UNITED STATES CIVIL(IAN)? DEFENSE OFFICE\b/, 'UNITED STATES OFFICE OF CIVIL\1 DEFENSE'); 
-  
   ag.gsub!(/ +/, ' '); # whitespace
   ag.sub!(/^ +/,  '');
   ag.sub!(/ +$/,  '');
