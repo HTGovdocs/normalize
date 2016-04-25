90bd82eb16a78450e08a8438a9d5693a2ff056b3
Joshua Steverman <jstever@umich.edu>
2016-04-25 15:55:02 -0400

    forgotten include of enum_chron

--- lib/normalize.rb
+++ lib/normalize.rb
@@ -4,4 +4,5 @@ require File.dirname(__FILE__) + '/normalize/version'
 require File.dirname(__FILE__) + '/normalize/corporate'
 require File.dirname(__FILE__) + '/normalize/lccn'
 require File.dirname(__FILE__) + '/normalize/title'
+require File.dirname(__FILE__) + '/normalize/enum_chron'
 
