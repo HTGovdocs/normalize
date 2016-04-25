4ac3b555c9546abf5fcf22f8ca376353cc47ec8e
Joshua Steverman <jstever@umich.edu>
2016-04-25 14:38:37 -0400

    empty test for Normalize.enum_chron.

---
+++ spec/normalize_enum_chron_spec.rb
@@ -0,0 +1,9 @@
+require 'normalize'
+
+RSpec.describe '#normalize_enum_chron' do
+  it "correctly normalizes" do
+    #expect(Normalize.enum_chron(" 79139101 /AC/r932")).to eq("79139101")
+  end
+end
+
+ 
